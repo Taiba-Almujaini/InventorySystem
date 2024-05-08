14d0f09c7b5691e69f0b4bae3dc1d1e1e943e58c
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-08 08:54:53 +0400

    add Inventory Controller methods

--- src/main/java/com/TRA/tra24Springboot/Controllers/InventoryController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InventoryController.java
@@ -1,7 +1,53 @@
 package com.TRA.tra24Springboot.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.TRA.tra24Springboot.Models.Inventory;
+import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Date;
+
 
 @RestController
+@RequestMapping("/product")
 public class InventoryController {
+
+    Inventory newInventory = new Inventory();
+
+
+    @PostMapping("receive")
+    public Inventory receiveNewStock(){
+        Inventory receiveInventory = new Inventory();
+
+        Product product = new Product();
+        product.setId(1);
+        product.setCreatedDate(new Date());
+
+        ProductDetails productDetails = new ProductDetails();
+        productDetails.setId(1);
+        productDetails.setCreatedDate(new Date());
+
+        product.setProductDetails(productDetails);
+
+        receiveInventory.setProducts(Arrays.asList(product));
+
+        receiveInventory.setLocation("Muscat");
+        receiveInventory.setManager("ALi");
+        receiveInventory.setPhoneNumber("99999999");
+        receiveInventory.setSupplier("Toyota");
+        receiveInventory.setOpeningHours("8 AM");
+        receiveInventory.setClosingHours("8 PM");
+
+       newInventory=receiveInventory;
+        return receiveInventory;
+    }
+
+    @GetMapping("return")
+    public  Inventory returnStock(){
+        return  newInventory;
+    }
+
+ 
+
 }
