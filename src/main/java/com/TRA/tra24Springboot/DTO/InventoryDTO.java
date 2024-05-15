37c08c6e1151c6ee59f64088adfd3674f4a2436d
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-15 12:17:21 +0400

    Added InventoryDTO methods

--- src/main/java/com/TRA/tra24Springboot/DTO/InventoryDTO.java
+++ src/main/java/com/TRA/tra24Springboot/DTO/InventoryDTO.java
@@ -1,4 +1,40 @@
 package com.TRA.tra24Springboot.DTO;
 
+import com.TRA.tra24Springboot.Models.Inventory;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class InventoryDTO {
+    Integer inventoryId;
+    List<ProductDTO>products;
+    String location;
+
+
+
+    public static InventoryDTO convertToDTO(Inventory inventory) {
+        InventoryDTO inventoryDTO = new InventoryDTO();
+        inventoryDTO.setInventoryId(inventory.getId());
+        inventoryDTO.setLocation(inventory.getLocation());
+        inventoryDTO.setProducts(ProductDTO.convertToDTO(inventory.getProducts()));
+
+        return inventoryDTO;
+    }
+
+    public static List<InventoryDTO> convertToDTO(List<Inventory> inventoryListList) {
+        List<InventoryDTO> orderDTO = new ArrayList<>();
+
+        for (Inventory inventoryDetailsDB : inventoryListList) {
+            InventoryDTO dto = convertToDTO(inventoryDetailsDB);
+            orderDTO.add(dto);
+        }
+
+        return orderDTO;
+    }
+
 }
+
+
