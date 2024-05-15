7a1f39524d4f1eb8e1cae3b605001a2220856038
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-15 09:50:04 +0400

    create ProductDetails Repository

---
+++ src/main/java/com/TRA/tra24Springboot/Repositories/ProductDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductDetailsRepository extends JpaRepository<ProductDetails,Integer> {
+
+
+}
