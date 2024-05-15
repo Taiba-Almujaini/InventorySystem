97cf17cf478ae0b664cc65b0260294a6e6d4f076
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-15 09:46:02 +0400

    ِAdded required annotations

--- src/main/java/com/TRA/tra24Springboot/Models/Inventory.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Inventory.java
@@ -1,17 +1,23 @@
 package com.TRA.tra24Springboot.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Entity
+
 public class Inventory extends BaseEntity {
 
+    @OneToMany
     List<Product> products;
     String location;
-    String manager; //TODO: Update once user class created
-    List<String> workers; //TODO: Update user class created
-    String supplier; //TODO: Update once supplier class created
+    String manager;
+    @OneToMany
+    List<Employee> workers;
+    String supplier;
     String phoneNumber;
     String openingHours;
     String closingHours;
--- src/main/java/com/TRA/tra24Springboot/Models/Order.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Order.java
@@ -1,19 +1,28 @@
 package com.TRA.tra24Springboot.Models;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
+@Table(name = "orders")
 public class Order extends BaseEntity {
 
+    @OneToMany
     List<Product> productsOrdered;
     String categoryName;
     Date orderDate;
+
+    @Enumerated(EnumType.STRING)
     OrderStatus status;
     String description;
+    @Enumerated(EnumType.STRING)
     PaymentStatus paymentStatus;
+    @Enumerated(EnumType.STRING)
     PaymentType paymentType;
     Date dueDate;
 }
+
