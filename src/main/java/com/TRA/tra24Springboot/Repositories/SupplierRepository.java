e1c760843fb88d407272eadc7e245d654a3670da
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-15 09:50:40 +0400

    Create Supplier Repository

---
+++ src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
@@ -0,0 +1,7 @@
+package com.TRA.tra24Springboot.Repositories;
+
+import com.TRA.tra24Springboot.Models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier,Integer> {
+}
