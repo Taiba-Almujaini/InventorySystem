2e4d653a007e999eca387b61f3dec61e6eeac096
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-26 11:50:58 +0400

    Add getProductByName query

--- src/main/java/com/TRA/tra24Springboot/Repositories/ProductRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/ProductRepository.java
@@ -2,6 +2,14 @@ package com.TRA.tra24Springboot.Repositories;
 
 import com.TRA.tra24Springboot.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+    @Query("SELECT p FROM Product p WHERE p.productDetails.name = :productName")
+    List<Product> getProductByName(@Param("productName") String productName);
 }
