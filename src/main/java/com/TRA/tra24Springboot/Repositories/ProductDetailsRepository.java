192de1b94c9387a34118598de4d7d9d9c1a90673
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-26 11:46:49 +0400

    Update ProductDetails Reposistory

--- src/main/java/com/TRA/tra24Springboot/Repositories/ProductDetailsRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/ProductDetailsRepository.java
@@ -1,7 +1,10 @@
 package com.TRA.tra24Springboot.Repositories;
 
+import com.TRA.tra24Springboot.Models.Product;
 import com.TRA.tra24Springboot.Models.ProductDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductDetailsRepository extends JpaRepository<ProductDetails,Integer> {
 
