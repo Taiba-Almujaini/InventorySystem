2d1c127616065bbf73a2c3764175b4259b4b879f
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-27 12:00:00 +0400

    Added query methods  for fetching orders by ID, category name, order status, payment status, and payment type

--- src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/OrderRepository.java
@@ -1,7 +1,31 @@
 package com.TRA.tra24Springboot.Repositories;
 
-import com.TRA.tra24Springboot.Models.Order;
+import com.TRA.tra24Springboot.Models.*;
+import jdk.jshell.Snippet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    @Query("SELECT o FROM Order o WHERE o.id = :orderId")
+    Order getOrderById(@Param("orderId") Integer orderId);
+
+    @Query("SELECT o FROM Order o WHERE o.categoryName = :categoryName")
+    List<Order> getOrderByCategoryName(@Param("categoryName") String categoryName);
+
+    @Query("SELECT o FROM Order o WHERE o.status = :orderStatus")
+    List<Order> getOrderByOrderStatus(@Param("orderStatus") OrderStatus orderStatus);
+
+    @Query("SELECT o FROM Order o WHERE o.paymentStatus = :paymentStatus")
+    List<Order> getOrderByPaymentStatus(@Param("paymentStatus") PaymentStatus paymentStatus);
+
+    @Query("SELECT o FROM Order o WHERE o.paymentType = :paymentType")
+    List<Order> getOrderByPaymentType(@Param("paymentType") PaymentType paymentType);
+
+
+
+
 }
