cfd8b5e9d212ac84bf34c04b0781da7e234657f8
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-27 12:21:49 +0400

    Add query  for fetching supplier by ID

--- src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/SupplierRepository.java
@@ -1,7 +1,18 @@
 package com.TRA.tra24Springboot.Repositories;
 
+import com.TRA.tra24Springboot.Models.Order;
 import com.TRA.tra24Springboot.Models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SupplierRepository extends JpaRepository<Supplier,Integer> {
+
+    @Query("SELECT s FROM Supplier s WHERE s.id = :supplierId")
+    Supplier getSupplierById(@Param("supplierId") Integer supplierId);
+
+
+
 }
