e49b7e5a1ffdb17a7d3c72a674955b2d89b7c3fc
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-28 10:04:38 +0400

    Added queryto get inventory by location

--- src/main/java/com/TRA/tra24Springboot/Repositories/InventoryRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repositories/InventoryRepository.java
@@ -1,7 +1,20 @@
 package com.TRA.tra24Springboot.Repositories;
 
 import com.TRA.tra24Springboot.Models.Inventory;
+import com.TRA.tra24Springboot.Models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository <Inventory, Integer> {
+    @Query("SELECT i FROM Inventory i WHERE i.id = :inventoryId")
+    Inventory getInventoryById(@Param("inventoryId") Integer inventoryId);
+
+    @Query("SELECT i FROM Inventory i WHERE i.location = :inventoryLocation")
+    List<Inventory> getInventoryByLocation(@Param("inventoryLocation") String inventoryLocation);
+
+
+
 }
