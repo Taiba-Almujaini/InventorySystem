2fd46f6f7d1f8b1a87dd302cd92bba0e7e0f9edf
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-05-29 12:15:38 +0400

    Added SlackService class to send messages to Slack channel

---
+++ src/main/java/com/TRA/tra24Springboot/Services/SlackService.java
@@ -0,0 +1,39 @@
+package com.TRA.tra24Springboot.Services;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SlackService {
+
+
+    @Value("${slack.token}")
+    private String slackToken;
+
+    public void sendMessage(String channel, String message) {
+        Slack slack = Slack.getInstance();
+
+
+        channel = "#taiba";
+        message = " ..";
+        ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                .channel(channel)
+                .text(message)
+                .build();
+
+        try {
+            ChatPostMessageResponse response = slack.methods(slackToken).chatPostMessage(request);
+            if (response.isOk()) {
+                System.out.println("Message sent successfully to Slack!");
+            } else {
+                System.out.println("Failed to send message to Slack: " + response.getError());
+            }
+        } catch (Exception e) {
+            System.out.println("Error sending message to Slack: " + e.getMessage());
+        }
+    }
+}
