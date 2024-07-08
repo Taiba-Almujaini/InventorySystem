5c63b810d80931d82e443b988a7566088c17478f
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-07-08 10:03:48 +0400

    Added methods to  getInvoiceByDueDate and getInvoiceDueInNextDays

--- src/main/java/com/TRA/tra24Springboot/Services/InvoiceService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/InvoiceService.java
@@ -3,11 +3,13 @@ package com.TRA.tra24Springboot.Services;
 import com.TRA.tra24Springboot.Models.Invoice;
 import com.TRA.tra24Springboot.Models.Product;
 import com.TRA.tra24Springboot.Repositories.InvoiceRepository;
+import com.TRA.tra24Springboot.Utils.DateHelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class InvoiceService {
@@ -25,10 +27,24 @@ public class InvoiceService {
         invoice.setTotalAmount(Double.parseDouble("36"));
         invoice.setIsActive(Boolean.TRUE);
         invoice.setPaidAmount(Double.parseDouble("89"));
+        Date dueDate=DateHelperUtils.addDays(invoice.getCreatedDate(),1);
 
         return  invoiceRepository.save(invoice);
 
     }
-
+    public Invoice getBInvoiceById(Integer id) {
+        return invoiceRepository.getInvoiceById(id);
+    }
+    public List<Invoice> getInvoiceByDueDate(Date dueDate) {
+        return invoiceRepository.getInvoiceByDueDate(dueDate);
+    }
+    // method to get invoices due in next few days
+    public List<Invoice> getInvoiceDueInNextDays(Integer days){
+        Date today = new Date();
+        Date dueDate = DateHelperUtils.addDays(today, days);
+        return invoiceRepository.getInvoicesByDueDateBetween(today, dueDate);
+    }
 
 }
+
+
