1863feeba0ef82aa7b7e5668d7a8e6cbf217b8b4
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-07-08 10:04:49 +0400

    Added method  to sendDueDateReminder

--- src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
@@ -7,9 +7,12 @@ import com.TRA.tra24Springboot.Repositories.InvoiceRepository;
 import com.TRA.tra24Springboot.Services.InvoiceService;
 import com.TRA.tra24Springboot.Services.SlackService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/invoice")
@@ -25,5 +28,19 @@ public class InvoiceController {
     public Invoice createInvoice(Invoice invoice) throws Exception {
         slackService.sendMessage("taiba","new invoice created");
         return invoiceService.createInvoice(invoice);
+    }
+    //@Scheduled(cron = "0 0 9 * * ?")
+    @PostMapping("dueDate")
+    public void senDueDateReminder() {
+        Integer remainingDays = 1;
+        List<Invoice> invoices = invoiceService.getInvoiceDueInNextDays(remainingDays);
+        for (Invoice invoice : invoices) {
+            StringBuilder message = new StringBuilder();
+            message.append("Reminder: Invoice #")
+                    .append(invoice.getId())
+                    .append(" is due on ")
+                    .append(invoice.getDueDate().toString());
+            slackService.sendMessage("practice", message.toString());
+        }
     }
 }
