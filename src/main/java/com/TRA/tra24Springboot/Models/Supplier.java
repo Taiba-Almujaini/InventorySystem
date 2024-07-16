32c986f8e6add5813cf258ffb077c43f2937496e
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-07-16 11:57:52 +0400

    Added Builder

--- src/main/java/com/TRA/tra24Springboot/Models/Supplier.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Supplier.java
@@ -4,13 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Supplier extends BaseEntity{
 
     String companyName;
