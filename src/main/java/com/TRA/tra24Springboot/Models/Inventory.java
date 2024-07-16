5785162bfea8fedbf5060205b7741937802c72bb
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-07-16 11:57:30 +0400

    Added builder

--- src/main/java/com/TRA/tra24Springboot/Models/Inventory.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Inventory.java
@@ -2,13 +2,18 @@ package com.TRA.tra24Springboot.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Entity
-
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inventory extends BaseEntity {
 
     @OneToMany
