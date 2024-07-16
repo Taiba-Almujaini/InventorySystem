f1af54c2648d60cf48e760add31738a5930eb040
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-07-16 11:57:10 +0400

    Added builder

--- src/main/java/com/TRA/tra24Springboot/Models/Invoice.java
+++ src/main/java/com/TRA/tra24Springboot/Models/Invoice.java
@@ -2,12 +2,18 @@ package com.TRA.tra24Springboot.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Invoice extends BaseEntity{
 
     double totalAmount;
