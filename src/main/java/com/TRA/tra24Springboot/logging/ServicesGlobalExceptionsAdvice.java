73b91dcfecb7c88e3c6914a92263fc1593f53ceb
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-07-24 11:41:12 +0400

    Create Global Exception Handling Advice for Services

---
+++ src/main/java/com/TRA/tra24Springboot/logging/ServicesGlobalExceptionsAdvice.java
@@ -0,0 +1,45 @@
+package com.TRA.tra24Springboot.logging;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServicesGlobalExceptionsAdvice {
+    private static final Logger logger = LoggerFactory.getLogger(ServicesGlobalExceptionsAdvice.class);
+
+    @Pointcut("execution(* com.TRA.tra24Springboot.Services.*.*(..))")
+    public void serviceLayerPointcut() {
+    }
+
+    @AfterThrowing(pointcut = "serviceLayerPointcut()", throwing = "ex")
+    public void handleGlobalException(Exception ex) {
+        logger.error("Exception caught in GlobalExceptionHandlingAspect: ", ex);
+
+        if (ex instanceof NullPointerException) {
+            handleNullPointerException((NullPointerException) ex);
+        } else if (ex instanceof IllegalArgumentException) {
+            handleIllegalArgumentException((IllegalArgumentException) ex);
+        } else {
+            handleGenericException(ex);
+        }
+    }
+
+    private void handleNullPointerException(NullPointerException ex) {
+        logger.error("NullPointerException occurred: ", ex);
+    }
+
+    private void handleIllegalArgumentException(IllegalArgumentException ex) {
+        logger.error("IllegalArgumentException occurred: ", ex);
+    }
+
+    private void handleGenericException(Exception ex) {
+        logger.error("An unexpected exception occurred: ", ex);
+    }
+
+
+}
